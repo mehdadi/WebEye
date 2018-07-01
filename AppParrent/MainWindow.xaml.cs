eb6c03537de419c0387825df5cbd0054debb442f
Mehrdad Montazeri <mehdadi@gmail.com>
2018-07-01 23:25:00 +0200

    my commit to prototype.

---
+++ AppParrent/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppParrent
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool init = false;
+        Process Others = new Process();
+        IntPtr id;
+        public MainWindow()
+        {
+            InitializeComponent();
+            LocationChanged += MainWindow_LocationChanged;
+            SizeChanged += MainWindow_SizeChanged;
+        }
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            SendCommand("setPosition", $"{(int)Application.Current.MainWindow.Top + 28}", $"{(int)Application.Current.MainWindow.Left}", $"{(int)Application.Current.MainWindow.Width}", $"{(int)Application.Current.MainWindow.Height}");
+        }
+
+        private void SendCommand(params string[] args)
+        {
+            if (!init) return;
+            string ex = string.Join(" ", args);
+            Console.WriteLine("Sending Command: {0}", ex);
+            Others?.StandardInput.WriteLine(ex);
+        }
+
+        private void MainWindow_LocationChanged(object sender, EventArgs e)
+        {
+            if (!init) return;
+            SendCommand("setPosition", $"{(int)Application.Current.MainWindow.Top + 28}", $"{(int)Application.Current.MainWindow.Left}", $"598", "600");
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            Others.StartInfo = new ProcessStartInfo(@"C:\Users\mehdadi\Desktop\WebEye\StreamPlayerControl\WPF\StreamPlayerDemo\StreamPlayerDemo\bin\Debug\StreamPlayerDemo.exe");
+            Others.StartInfo.RedirectStandardError = Others.StartInfo.RedirectStandardInput = Others.StartInfo.RedirectStandardOutput = true;
+            Others.StartInfo.UseShellExecute = false;
+            Others.Start();
+            System.Threading.Thread.Sleep(2000);
+            id = Others.MainWindowHandle;
+
+            Title = "Running";
+            init = true;
+            MainWindow_LocationChanged(null, null);
+            Others.Exited += Others_Exited;
+
+            Console.Write(id);
+            thr = new Thread(new ThreadStart(() =>
+            {
+                while (true)
+                {
+                    //System.Threading.Thread.Sleep(2000);
+                    string cmd = Others.StandardOutput.ReadLine();
+                    Console.WriteLine(cmd);
+                }
+            }));
+            thr.Start();
+
+        }
+
+        private void Others_Exited(object sender, EventArgs e)
+        {
+
+        }
+
+        Thread thr;
+        protected override void OnClosed(EventArgs e)
+        {
+            if (!init) return;
+            Others.Kill();
+            thr.Abort();
+            base.OnClosed(e);
+        }
+
+        private void play_Click(object sender, RoutedEventArgs e)
+        {
+            SendCommand("play");
+        }
+
+        private void stop_Click(object sender, RoutedEventArgs e)
+        {
+            SendCommand("stop");
+        }
+    }
+}
--- StreamPlayerControl/WPF/StreamPlayerDemo/StreamPlayerDemo/MainWindow.xaml.cs
+++ StreamPlayerControl/WPF/StreamPlayerDemo/StreamPlayerDemo/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using Microsoft.Win32;
+using System.Linq;
 
 namespace StreamPlayerDemo
 {
@@ -9,16 +10,72 @@ namespace StreamPlayerDemo
     /// </summary>
     public partial class MainWindow
     {
+        System.Threading.Thread Tmsg;
         public MainWindow()
         {
             InitializeComponent();
+            Tmsg = new System.Threading.Thread(new System.Threading.ThreadStart(() =>
+            {
+                while (true)
+                {
+                    //System.Threading.Thread.Sleep(2000);
+                    string command = Console.ReadLine();
+                    Console.WriteLine("command recieved: {0}", command);
+                    string[] cmds = command.Split(' ');
+                    switch(cmds[0])
+                    {
+                        case "play":
+                            Dispatcher.Invoke(() =>
+                            {
+                                HandlePlayButtonClick(null, null);
+                            });
+                            break;
+                        case "setPosition":
+                            Dispatcher.Invoke(() =>
+                            {
+                                SetPosition(cmds);
+                            });
+                            break;
+                        default:
+                            Console.WriteLine("Command not supported.");
+                            break;
+                    }
+                }
+            }));
+            Tmsg.Start();
+        }
+
+        private void SetPosition(string[] cmds)
+        {
+            string[] d = new string[cmds.Length - 1];
+            Array.Copy(cmds, 1, d, 0, cmds.Length - 1);
+            int[] values = Array.ConvertAll(d, int.Parse);
+            this.Top = values[0];
+            this.Left = values[1];
+            this.Width = values[2];
+            this.Height = values[3];
+            BringIntoView();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            Tmsg.Abort();
         }
 
         private void HandlePlayButtonClick(object sender, RoutedEventArgs e)
         {
+            Console.WriteLine("command executing: Connecting {0}", _urlTextBox.Text);
             var uri = new Uri(_urlTextBox.Text);
-            _streamPlayerControl.StartPlay(uri);
-            _statusLabel.Text = "Connecting...";
+            try
+            {
+                _streamPlayerControl.StartPlay(uri);
+                _statusLabel.Text = "Connecting...";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("command Failed: Connecting {0}", ex);
+            }
         }
 
         private void HandleStopButtonClick(object sender, RoutedEventArgs e)
@@ -37,9 +94,9 @@ namespace StreamPlayerDemo
 
         private void UpdateButtons()
         {
-            _playButton.IsEnabled = !_streamPlayerControl.IsPlaying; 
-            _stopButton.IsEnabled = _streamPlayerControl.IsPlaying;
-            _imageButton.IsEnabled = _streamPlayerControl.IsPlaying;
+            //    _playButton.IsEnabled = !_streamPlayerControl.IsPlaying; 
+            //    _stopButton.IsEnabled = _streamPlayerControl.IsPlaying;
+            //    _imageButton.IsEnabled = _streamPlayerControl.IsPlaying;
         }
 
         private void HandlePlayerEvent(object sender, RoutedEventArgs e)
